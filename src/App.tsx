71fb97212ccc08d2843d9a428019bf97b62af679
Angel Fernandes <angelfernandes07@hotmail.com>
2021-02-24 14:47:43 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyle from './Styles/global';
+import SignIn from './pages/SignIn';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
